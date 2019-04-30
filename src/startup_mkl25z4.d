9a057a15868c0cec26312d4e8ff08c0d5ebd0049
OneMoreByte <jack@jackhil.de>
2019-04-30 17:29:59 -0500

    cleanup

--- src/startup_mkl25z4.d
+++
@@ -1,1 +0,0 @@
-.\startup_mkl25z4.o: RTE\Device\MKL25Z32xxx4\startup_MKL25Z4.s
--- src/system_mkl25z4.d
+++
@@ -1,8 +0,0 @@
-.\system_mkl25z4.o: RTE\Device\MKL25Z32xxx4\system_MKL25Z4.c
-.\system_mkl25z4.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\system_mkl25z4.o: C:\Keil_v5\ARM\PACK\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL25Z4.h
-.\system_mkl25z4.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\core_cm0plus.h
-.\system_mkl25z4.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_version.h
-.\system_mkl25z4.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_compiler.h
-.\system_mkl25z4.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_armcc.h
-.\system_mkl25z4.o: .\RTE\Device\MKL25Z32xxx4\system_MKL25Z4.h
