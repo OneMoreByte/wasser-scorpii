70bc3231784c2473bf6b1ecf5123f28ba6fb7f86
MaximUltimatum <mcder018@live.com>
2019-04-30 18:16:54 -0500

    Added a basic skeleton for main function

--- src/main.c
+++ src/main.c
@@ -1,7 +1,26 @@
 #include <MKL25Z4.H>
 
+//Global variables
+int stepcount = 0;
 
 int main(void) {
-
+	int degree = stepcount%360;
+	
+	/*
+	While loop turns motor in a circle whilst calling isTriggered();
+	*/
+	while(1){
+		//*run motor*
+		if(isTriggered()){
+			stopNfire();
+		}
+	}
 
 }
+
+void stopNfire(){
+	//send power to a port
+	//LED for test case, massive pump for real product
+}
+
+
